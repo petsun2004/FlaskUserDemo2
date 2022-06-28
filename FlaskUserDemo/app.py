c40a7cc463690dcd1783906e27f706d909a5d78c
Peter Sun <petsun@student.pakuranga.school.nz>
2022-06-29 10:41:01 +1200

    Add project files.

---
+++ FlaskUserDemo/app.py
@@ -0,0 +1,171 @@
+import uuid, os, hashlib
+from flask import Flask, request, render_template, redirect, session, abort, flash
+app = Flask(__name__)
+
+# Register the setup page and import create_connection()
+from utils import create_connection, setup
+app.register_blueprint(setup)
+
+@app.before_request
+def restrict():
+    restricted_pages = [
+        'list_users',
+        'view_user',
+        'edit_user',
+        'delete_user'
+    ]
+    if 'logged_in' not in session and request.endpoint in restricted_pages:
+        flash("You must be logged in to view this page.")
+        return redirect('/login')
+
+@app.route('/')
+def home():
+    return render_template("login.html")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+
+        password = request.form['password']
+        encrypted_password = hashlib.sha256(password.encode()).hexdigest()
+
+        print(encrypted_password)
+
+        with create_connection() as connection:
+            with connection.cursor() as cursor:
+                sql = "SELECT * FROM user_infor WHERE email=%s AND password=%s"
+                values = (
+                    request.form['email'],
+                    encrypted_password
+                )
+                cursor.execute(sql, values)
+                result = cursor.fetchone()
+        if result:
+            session['logged_in'] = True
+            session['first_name'] = result['first_name']
+            session['role'] = result['role']
+            session['user_id'] = result['user_id']
+            return redirect("/dashboard")
+        else:
+            flash("Invalid username or password.")
+            return redirect("/login")
+    else:
+        return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
+
+@app.route('/register', methods=['GET', 'POST'])
+def add_user():
+    if request.method == 'POST':
+
+        password = request.form['password']
+        encrypted_password = hashlib.sha256(password.encode()).hexdigest()
+
+        with create_connection() as connection:
+            with connection.cursor() as cursor:
+                sql = """INSERT INTO users
+                    (first_name, last_name, email, password, avatar)
+                    VALUES (%s, %s, %s, %s, %s)
+                """
+                values = (
+                    request.form['first_name'],
+                    request.form['last_name'],
+                    request.form['email'],
+                    encrypted_password,
+                )
+                cursor.execute(sql, values)
+                connection.commit()
+        return redirect('/')
+    return render_template('users_add.html')
+
+@app.route('/dashboard')
+def list_users():
+    if session['role'] != 'admin':
+        flash("Only admin can access this page.")
+        return redirect('/')
+    with create_connection() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM user_infor")
+            result = cursor.fetchall()
+    return render_template('users_list.html', result=result)
+
+@app.route('/view')
+def view_user():
+    with create_connection() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM user_infor WHERE id=%s", request.args['id'])
+            result = cursor.fetchone()
+    return render_template('users_view.html', result=result)
+
+@app.route('/delete')
+def delete_user():
+    with create_connection() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute("DELETE FROM user_infor WHERE id=%s", request.args['id'])
+            connection.commit()
+    return redirect('/dashboard')
+
+@app.route('/edit', methods=['GET', 'POST'])
+def edit_user():
+    # Admin are allowed, users with the right id are allowed, everyone else sees 404.
+    if session['role'] != 'admin' and str(session['id']) != request.args['id']:
+        flash("You don't have permission to edit this user.")
+        return redirect('/view?id=' + request.args['id'])
+
+    if request.method == 'POST':
+        if request.files['avatar'].filename:
+            avatar_image = request.files["avatar"]
+            ext = os.path.splitext(avatar_image.filename)[1]
+            avatar_filename = str(uuid.uuid4())[:8] + ext
+            avatar_image.save("static/images/" + avatar_filename)
+            if request.form['old_avatar'] != 'None':
+                os.remove("static/images/" + request.form['old_avatar'])
+        elif request.form['old_avatar'] != 'None':
+            avatar_filename = request.form['old_avatar']
+        else:
+            avatar_filename = None
+
+        with create_connection() as connection:
+            with connection.cursor() as cursor:
+                sql = """UPDATE user_infor SET
+                    first_name = %s,
+                    last_name = %s,
+                    email = %s,
+                    avatar = %s
+                WHERE id = %s"""
+                values = (
+                    request.form['first_name'],
+                    request.form['last_name'],
+                    request.form['email'],
+                    avatar_filename,
+                    request.form['id']
+                )
+                cursor.execute(sql, values)
+                connection.commit()
+        return redirect('/view?id=' + request.form['id'])
+    else:
+        with create_connection() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT * FROM user_infor WHERE id = %s", request.args['id'])
+                result = cursor.fetchone()
+        return render_template('users_edit.html', result=result)
+
+
+
+if __name__ == '__main__':
+    import os
+
+    # This is required to allow sessions.
+    app.secret_key = os.urandom(32)
+
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT, debug=True)
+
